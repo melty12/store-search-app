af28975a5753cf3f4b4e39c70fb61a411d410991
HirosueAkari <hirosue_akari@retail-ai.jp>
2021-09-06 11:27:53 +0900

    eslint設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-    "plugins": ["prettier"],
-    "extends": ["plugin:react/recommended"],
-    "rules": {
-        "prettier/prettier": "error"
-    }
+  plugins: ["prettier"],
+  extends: ["plugin:react/recommended"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2015,
+  },
+  parser: "babel-eslint",
 };
